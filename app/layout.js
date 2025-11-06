cb81e5d249b6adc86567589a69f1144098d1f064
Santosh Patel <santoshpatelvns5@gmail.com>
2025-11-06 23:52:30 +0530

    fix cleark provider

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs/app-beta";
+import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Welth",
