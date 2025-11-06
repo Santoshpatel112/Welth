056b64b272d958a1c780f7bcdce0ab5bc34f0516
Santosh Patel <santoshpatelvns5@gmail.com>
2025-11-07 00:01:12 +0530

    cleark setup

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import { Button } from "../components/ui/button";
 export default function Home() {
   return (
     <>
-    <h1>Home Page</h1>
-    <Button variant="">Click Me</Button>
+    
     </>
   );
 }
